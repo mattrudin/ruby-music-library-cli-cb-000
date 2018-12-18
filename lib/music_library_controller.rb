c50ea81071d8bf707025b52bab3fa8f4eae379b5
Matthias Rudin <matthias.rudin@live.com>
2018-12-18 18:40:09 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -5,7 +5,7 @@ class MusicLibraryController
   end
 
   def call
-    user_input = ""
+    @user_input = ""
     if user_input != "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
@@ -16,7 +16,7 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-      user_input = gets.chomp
+      @user_input = gets.chomp
     end
   end
 
